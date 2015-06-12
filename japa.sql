f4c9caf1c31d33aaaa1981be980488c73596bc06
Henrique Nunweiler <henriqueprogramacao@gmail.com>
2015-06-12 09:36:28 -0300

    Banco de Dados Atualizado
    Adicionado registro Usuário Viviane.

--- japa.sql
+++ japa.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tempo de Geração: Jun 10, 2015 as 04:11 AM
+-- Tempo de Geração: Jun 12, 2015 as 02:35 PM
 -- Versão do Servidor: 5.5.10
 -- Versão do PHP: 5.3.6
 
@@ -22,40 +22,24 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `fornecedor`
+-- Estrutura da tabela `usuario`
 --
 
-CREATE TABLE IF NOT EXISTS `fornecedor` (
-  `for_id` int(11) NOT NULL AUTO_INCREMENT,
-  `for_CNPJ` varchar(20) NOT NULL,
-  `for_razao_social` varchar(50) NOT NULL,
-  `for_nome_fantasia` varchar(40) NOT NULL,
-  `for_inscricao_estadual` varchar(25) NOT NULL,
-  `for_rua` varchar(25) NOT NULL,
-  `for_numero` int(11) NOT NULL,
-  `for_bairro` varchar(20) NOT NULL,
-  `for_cidade` varchar(30) NOT NULL,
-  `for_estado` varchar(15) NOT NULL,
-  `for_email` varchar(30) NOT NULL,
-  `for_telefone1` varchar(20) NOT NULL,
-  `for_telefone2` varchar(20) NOT NULL,
-  `for_celular` varchar(20) NOT NULL,
-  `for_id_nextel` varchar(10) NOT NULL,
-  `for_data_cadastro` varchar(10) NOT NULL,
-  `for_nome_banco_1` varchar(10) NOT NULL,
-  `for_agencia_1` varchar(10) NOT NULL,
-  `for_conta_corrente_1` varchar(10) NOT NULL,
-  `for_nome_banco_2` varchar(10) NOT NULL,
-  `for_agencia_2` varchar(10) NOT NULL,
-  `for_conta_corrente_2` varchar(10) NOT NULL,
-  `for_vendedor` varchar(15) NOT NULL,
-  `for_telefone_vendedor` varchar(20) NOT NULL,
-  `for_email_vendedor` varchar(30) NOT NULL,
-  `for_observacoes` varchar(100) NOT NULL,
-  PRIMARY KEY (`for_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `usu_id` int(11) NOT NULL AUTO_INCREMENT,
+  `usu_nome` varchar(45) DEFAULT NULL,
+  `usu_usuario` varchar(50) NOT NULL,
+  `usu_senha` varchar(50) NOT NULL,
+  `usu_setor` varchar(20) DEFAULT NULL,
+  `usu_acesso` char(1) DEFAULT NULL COMMENT '0 = Administrativo    -       1 = Gerente',
+  PRIMARY KEY (`usu_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
 
 --
--- Extraindo dados da tabela `fornecedor`
+-- Extraindo dados da tabela `usuario`
 --
 
+INSERT INTO `usuario` (`usu_id`, `usu_nome`, `usu_usuario`, `usu_senha`, `usu_setor`, `usu_acesso`) VALUES
+(8, 'Henrique Nunweiler', 'nunweiler', '12345', 'Invicta', '1'),
+(10, 'Matheus Henrique', 'matherique', '12345', 'Programação', '1'),
+(11, 'Viviane', 'viviane', '12345', 'Financeiro', '1');
