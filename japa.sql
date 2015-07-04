96d8a891e568108899b203e655c0e0f83ebb71d6
Matheus Henrique <matherique@gmail.com>
2015-07-04 05:31:31 -0300

    Alteracoes no banco de dados

--- japa.sql
+++ japa.sql
@@ -1,45 +1,33 @@
--- phpMyAdmin SQL Dump
--- version 3.3.9.2
--- http://www.phpmyadmin.net
---
--- Servidor: localhost
--- Tempo de Geração: Jun 12, 2015 as 02:35 PM
--- Versão do Servidor: 5.5.10
--- Versão do PHP: 5.3.6
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Banco de Dados: `japa`
---
-
+-- --------------------------------------------------------
+-- Servidor:                     127.0.0.1
+-- Versão do servidor:           5.6.16 - MySQL Community Server (GPL)
+-- OS do Servidor:               Win32
+-- HeidiSQL Versão:              9.2.0.4947
 -- --------------------------------------------------------
 
---
--- Estrutura da tabela `usuario`
---
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+-- Copiando dados para a tabela japa.cliente: ~2 rows (aproximadamente)
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+REPLACE INTO `cliente` (`cli_id`, `cli_nome`, `cli_data_nascimento`, `cli_rg`, `cli_cpf`, `cli_cnpj`, `cli_nome_fantasia`, `cli_razao_social`, `cli_inscricao_estadual`, `cli_rua`, `cli_numero`, `cli_bairro`, `cli_cidade`, `cli_cep`, `cli_telefone1`, `cli_telefone2`, `cli_celular`, `cli_id_nextel`, `cli_email`, `cli_nome_responsavel`, `cli_data_cadastro`, `cli_porcentagem_desconto`, `cli_limite_compra`, `cli_observacoes`, `fk_ram_id`) VALUES
+	(2, 'Matheus Henrique dos Santos', '18/11/2015', '383891334', '123123123', '11.111.111/1111-11', 'WebChrnos', 'WebChronos TI', '123', 'Rua Jose Pacini', 137, 'Topolandia', 'Sao Sebastiao', '11600-000', '(12)9976-4452', '(12)1111-1111', '(11)1111-1111', '12qs', 'matherique@gmail.com', 'Matheus', '10/06/2015', '10%', 100, 'SEILKSDASKDKASDKASDASDASDASD', 1),
+	(4, 'Matheus Henrique', '18/11/2015', '383891334', '123123', '11.111.111/1111-11', 'WebChrnos', 'WebChronos TI', '123', 'Rua Jose Pacini', 137, 'Topolandia', 'Sao Sebastiao', '11600-000', '(12)9976-4452', '(12)1111-1111', '(11)1111-1111', '12qs', 'matherique@gmail.com', '', '', '10%', 100, 'SEILKSDASKDKASDKASDASDASDASD', 1);
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
 
-CREATE TABLE IF NOT EXISTS `usuario` (
-  `usu_id` int(11) NOT NULL AUTO_INCREMENT,
-  `usu_nome` varchar(45) DEFAULT NULL,
-  `usu_usuario` varchar(50) NOT NULL,
-  `usu_senha` varchar(50) NOT NULL,
-  `usu_setor` varchar(20) DEFAULT NULL,
-  `usu_acesso` char(1) DEFAULT NULL COMMENT '0 = Administrativo    -       1 = Gerente',
-  PRIMARY KEY (`usu_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+-- Copiando dados para a tabela japa.fornecedor: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `fornecedor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `fornecedor` ENABLE KEYS */;
 
---
--- Extraindo dados da tabela `usuario`
---
-
-INSERT INTO `usuario` (`usu_id`, `usu_nome`, `usu_usuario`, `usu_senha`, `usu_setor`, `usu_acesso`) VALUES
-(8, 'Henrique Nunweiler', 'nunweiler', '12345', 'Invicta', '1'),
-(10, 'Matheus Henrique', 'matherique', '12345', 'Programação', '1'),
-(11, 'Viviane', 'viviane', '12345', 'Financeiro', '1');
+-- Copiando dados para a tabela japa.usuario: ~4 rows (aproximadamente)
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+REPLACE INTO `usuario` (`usu_id`, `usu_nome`, `usu_usuario`, `usu_senha`, `usu_setor`, `usu_acesso`) VALUES
+	(6, 'Ana Paula', 'ana', '12345', 'Diretoria', '1'),
+	(8, 'Henrique Nunweiler', 'nunweiler', '12345', 'Invicta', '1'),
+	(9, 'Julio Cesar', 'julio', '12345', 'Programação', '1'),
+	(10, 'Matheus Henrique', 'matherique', '12345', 'Programação', '1');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
